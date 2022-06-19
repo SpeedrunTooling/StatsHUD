75f5982add0c69dead22d88e42039a7772971bf7
Mert <63981288+Mysterion06@users.noreply.github.com>
2022-06-19 07:59:49 +0200

    RE8 1.0.0.8
    - Added Jack Eye HP

--- index.js
+++ index.js
@@ -695,6 +695,16 @@ function ResidentEvil7(data)
 	let _colors = GetColor(data.Player);
 	DrawProgressBar(data.Player.CurrentHP, data.Player.MaxHP, data.Player.Percentage, "Player: ", _colors);
 	DrawTextBlocks(["Rank", "RankScore"], [data.Rank, data.RankScore], ["white", "green2"], HideDA);
+
+	// Jack Eyes
+	var filterdJackEyes = data.JackHP.filter(m => { return (m.IsAlive) });
+	filterdJackEyes.sort(function (a, b) {
+		return Asc(a.CurrentHP, b.CurrentHP) || Desc(a.CurrentHP, b.CurrentHP);
+	}).forEach(function (item, index, arr) {
+		DrawProgressBar(item.CurrentHP, item.CurrentHP, 1, "", ["danger", "red"]);
+	});
+
+	// Enemy HPs
 	var filterdEnemies = data.EnemyHealth.filter(m => { return (m.IsAlive) });
 	filterdEnemies.sort(function (a, b) {
 		return Asc(a.CurrentHP, b.CurrentHP) || Desc(a.CurrentHP, b.CurrentHP);
