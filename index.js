e537e3f5dafe63409f4bc511c22d54a52d09387e
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2024-06-21 21:21:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -473,22 +473,21 @@ function ResidentEvil1Remake(data)
 }
 
 // RESIDENT EVIL 2
-// CLASSIC 
-// TODO: FIX ENEMY HP
+// CLASSIC
 function ResidentEvil2Classic(data)
 {
 	DrawTextBlock("IGT", data.IGTFormattedString, ["white", "green2"], HideIGT);
 	let _colors = GetColor(data.Player);
 	DrawProgressBar(data.Player.CurrentHP, data.Player.MaxHP, data.Player.Percentage, data.PlayerName, _colors);
-	//var filterdEnemies = data.EnemyHealth.filter(m => { return (m.IsAlive) });
-	//filterdEnemies.sort(function (a, b) {
-	//	return Asc(a.CurrentHP, b.CurrentHP) || Desc(a.CurrentHP, b.CurrentHP);
-	//}).forEach(function (item, index, arr) {
-	//	if (item.MaximumHP != 0)
-	//		DrawProgressBar(item.CurrentHP, item.MaximumHP, item.Percentage, "", ["danger", "red"]);
-	//	else
-	//		DrawTextBlock("Enemy", item.CurrentHP, ["white", "red"], false);
-	//});
+	var filterdEnemies = data.EnemyHealth.filter(m => { return (m.IsAlive) });
+	filterdEnemies.sort(function (a, b) {
+		return Asc(a.CurrentHP, b.CurrentHP) || Desc(a.CurrentHP, b.CurrentHP);
+	}).forEach(function (item, index, arr) {
+		if (item.MaximumHP != 0)
+			DrawProgressBar(item.CurrentHP, item.MaximumHP, item.Percentage, "", ["danger", "red"]);
+		else
+			DrawTextBlock("Enemy", item.CurrentHP, ["white", "red"], false);
+	});
 }
 
 // REMAKE
